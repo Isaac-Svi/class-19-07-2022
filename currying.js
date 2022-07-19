b3855aaf02a131d0ecfbab6a7a91620371193a68
Isaac Svi <yitzchaksviridyuk@gmail.com>
2022-07-19 22:57:37 +0300

    add currying screenshot

--- currying.js
+++ currying.js
@@ -1,9 +1,19 @@
-// const add = a => b => a + b;
+// const some = a => b => a + b;
 
-const add = function (a) {
+const some = function (a) {
     const x = function (b) {
         return a + b;
     };
 
+    return x;
+};
+
+// The screenshot is for this function
+const add = function (num1) {
+    let counter = 0;
+    const x = function (num2) {
+        counter += 1 + num2 + num2;
+        return counter;
+    };
     return x;
 };
